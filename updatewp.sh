71a1b08ea5d59bbebce96158759a4358eebba13c
Felipe Pereira <felipe.psilveira@outlook.com>
2023-03-25 01:46:12 +0000

    Adicionei a atualização dos temas

--- updatewp.sh
+++ updatewp.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
 #########################################################################
-# Versao 0.1							                            	#
-# 							                                    		#
-# updatewp.sh - Atualizando o Wordpress de forma automatica	        	#
-#								                                       	#
-# Autor: Felipe Silveira (felipe.silveira@outlook.com)		        	#
-# Data Criação: 16/10/1997						                        #
-#									                                    #
-# Descrição: Criando uma rotina de atualização do Wordpress.	    	#
-#									                                    #
-# Exemplo de uso: bash updatewp.sh			                    		#
-#								                                    	#
+# Versao 0.1                                                                                    
+#                                                                                               
+# updatewp.sh - Atualizando o Wordpress de forma automatica                     
+#                                                                               
+# Autor: Felipe Silveira (felipe.silveira@outlook.com)                          
+# Data Criação: 11/03/2023                                                      
+#                                                                               
+# Descrição: Criando uma rotina de atualização do Wordpress.            
+#                                                                       
+# Exemplo de uso: bash updatewp.sh                                      
+#                                                                       
 #########################################################################
 
-##################### Variaveis #########################################
-# Logs da movimentação                                                  
-LOG=/var/log/updatewp.log                                               
+##################### Variaveis do Sistema ##############################
+# Logs da movimentação
+LOG=/var/log/updatewp.log
 
-# Variaveis do Wordpress                                                
-PastaWP=/var/www                                                        
-DownloadWP=$PastaWP/                                                    
+# Variaveis do Wordpress
+PastaWP=/var/www
+DownloadWP=$PastaWP/
 
-# Variavel do local dos plugins do Wordpress                            
-Plugins=/var/www/wordpress/wp-content/plugins/                          
+# Variavel do local dos plugins do Wordpress
+Plugins=/var/www/wordpress/wp-content/plugins/
+Themes=/var/www/wordpress/wp-content/themes/
+
+###########################################################################
+
+##################### Variaveis de Plugin #################################
+
+###### Akismet ######
 
 # Ver a versão mais nova do plugin do Akismet
 VersaoAkismet=$(curl -s https://br.wordpress.org/plugins/akismet/ | grep \"downloadUrl\": | awk -F "plugin" '{print $2}' | awk -F '"' '{print $1}') 2> /dev/null
@@ -34,6 +41,9 @@ AkismetZip=$(echo "$VersaoAkismet" | awk -F "/" '{print $2}') 2> /dev/null
 # Pega o nome do arquivo ZIP sem a /
 Akismet=$(echo $VersaoAkismet | awk -F "/" '{print $2}') 2> /dev/null
 
+
+###### Google site Kit ######
+
 # Ver a versão mais nova do plugin do Google site Kit
 VersaoGoogleKit=$(curl -s https://br.wordpress.org/plugins/google-site-kit/ | grep \"downloadUrl\": | awk -F "plugin" '{print $2}' | awk -F '"' '{print $1}') 2> /dev/null
 
@@ -43,7 +53,31 @@ GoogleKitZip=$(echo "$VersaoGoogleKit" | awk -F "/" '{print $2}') 2> /dev/null
 # Pega o nome do arquivo ZIP sem a /
 GoogleKit=$(echo $VersaoGoogleKit | awk -F "/" '{print $2}') 2> /dev/null
 
-#########################################################################
+##################### Variaveis de Tema #####################################
+
+###### Twenty Twenty-One ######
+
+# Ver a versão mais nova do tema Twenty Twenty-One
+VersaoTwentyOne=$(curl -s https://br.wordpress.org/themes/twentytwentyone/ | grep \"downloadUrl\": | awk -F "theme" '{print $2}' | awk -F '"' '{print $1}') 2> /dev/null
+
+# Pega o nome do arquivo ZIP com a /
+TwentyOneZip=$(echo "$VersaoTwentyOne" | awk -F "/" '{print $2}') 2> /dev/null
+
+# Pega o nome do arquivo ZIP sem a /
+TwentyOne=$(echo $VersaoTwentyOne | awk -F "/" '{print $2}') 2> /dev/null
+
+###### Daily Blog ######
+
+# Ver a versão mais nova do tema Daily Blog
+VersaoDailyBlog=$(curl -s https://br.wordpress.org/themes/daily-blog/ | grep \"downloadUrl\": | awk -F "theme" '{print $2}' | awk -F '"' '{print $1}') 2> /dev/null
+
+# Pega o nome do arquivo ZIP com a /
+DailyBlogZip=$(echo "$VersaoDailyBlog" | awk -F "/" '{print $2}') 2> /dev/null
+
+# Pega o nome do arquivo ZIP sem a /
+DailyBlog=$(echo $VersaoDailyBlog | awk -F "/" '{print $2}') 2> /dev/null
+
+###########################################################################
 
 # Iniciando o log da atualização #
 echo "Iniciando a atualização do Wordpress em $(date)" >> $LOG
@@ -68,7 +102,7 @@ mv $PastaWP/wordpress/ $PastaWP/wordpress_$VERSAO/ >> $LOG 2>&1
 echo "Extraindo a nova versão" >> $LOG
 tar -zxvf $PastaWP/latest.tar.gz >> $LOG 2>&1
 
-# Copiando o wp-content, .htaccess e wp-config da versão antiga para a nova # 
+# Copiando o wp-content, .htaccess e wp-config da versão antiga para a nova #
 echo "Copiando o WP-CONTENT da versão antiga para a nova" >> $LOG
 cp -r $PastaWP/wordpress_$VERSAO/wp-content/ $PastaWP/wordpress >> $LOG 2>&1
 echo "Copiando o WP-CONFIG da versão antiga para a nova" >> $LOG
@@ -101,6 +135,32 @@ cd $Plugins && wget https://downloads.wordpress.org/plugin/$VersaoGoogleKit >> $
 # Retira o zip do Google Site Kit e exclui o arquivo .zip #
 unzip -o $GoogleKitZip | awk -F "/" '{print $2}' >> $LOG 2>&1
 rm $GoogleKitZip >> $LOG 2>&1
+
+# Apaga a versão antiga do tema Twenty-One #
+echo "Apagando a versão antiga do tema Twenty Twenty-One" >> $LOG
+rm -rf $Themes/twentytwentyone/ >> $LOG 2>&1
+
+# Faz o download da versão mais atual do tema Twenty-One #
+echo "Download da versão mais atual do tema Twenty Twenty-One" >> $LOG
+cd $Themes && wget https://downloads.wordpress.org/theme$VersaoTwentyOne >> $LOG 2>&1
+
+# Retira o zip do tema Twenty-One e exclui o arquivo .zip #
+VersaoTwentyOne_=$(echo $VersaoTwentyOne | awk -F "/" '{print $2}') >> $LOG 2>&1
+unzip -o $VersaoTwentyOne_ >> $LOG 2>&1
+rm $VersaoTwentyOne >> $LOG 2>&1
+
+# Apaga a versão antiga do tema daily-blog #
+echo "Apagando a versão antiga do tema Twenty Twenty-One" >> $LOG
+rm -rf $Themes/daily-blog/ >> $LOG 2>&1
+
+# Faz o download da versão mais atual do tema daily-blog #
+echo "Download da versão mais atual do tema Twenty Twenty-One" >> $LOG
+cd $Themes && wget https://downloads.wordpress.org/theme$VersaoDailyBlog >> $LOG 2>&1
+
+# Retira o zip do tema daily-blog e exclui o arquivo .zip #
+VersaoDailyBlog_=$(echo $VersaoDailyBlog | awk -F "/" '{print $2}') >> $LOG 2>&1
+unzip -o $VersaoDailyBlog_ >> $LOG 2>&1
+rm $VersaoDailyBlog >> $LOG 2>&1
 
 # Reiniciando o apache #
 echo "Reiniciando o Apache" >> $LOG
